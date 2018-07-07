2f394d94f3eda9b700d336fdfbcc9a20e55b9f3a
Naoki <argon19951128@gmail.com>
2018-07-08 02:29:32 +0900

    add comment

--- makeLinks.bash
+++ makeLinks.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#usage
+#./makeLinks.bash or bash makeLinks.bash
+
+
 for file in `find -maxdepth 1 -mindepth 1 -name ".*" -not -name ".git"` ; do
    ln -sf $HOME/dotfiles/${file:2} $HOME/${file:2}
 done
